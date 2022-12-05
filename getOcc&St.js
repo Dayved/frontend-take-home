f67c68d2a8d58f643af09f99f2377b52f5863200
David <Dayved28@gmail.com>
2022-12-05 17:30:18 -0600

    occupation and state working

---
+++ getOcc&St.js
@@ -0,0 +1,42 @@
+$(document).ready(initApplication);
+
+function initApplication() {
+    var job = document.getElementById('occupation');
+    var US = document.getElementById('state');
+        
+        window.addEventListener("load", function () {
+            httpGet("https://frontend-take-home.fetchrewards.com/form");
+        });
+
+        function httpGet(theUrl){
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open( "GET", theUrl, false); // false for synchronous request
+            xmlHttp.setRequestHeader("Content-Type", "application/json");
+            xmlHttp.onreadystatechange = function () {
+                if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                    var json = JSON.parse(xmlHttp.responseText);
+                   // console.log(JSON.stringify(json, " ", 2));
+                    var occ = json.occupations;
+                    var st = json.states;
+                    //console.log(st.length);
+                    
+                for (let value of Object.values(occ)){
+                    var opt = document.createElement('option');
+                    opt.text = value;
+                    opt.value = value;
+                    job.appendChild(opt);
+                }
+
+                for (let value of Object.values(st)){
+                   // console.log(value.abbreviation);
+                    var opt = document.createElement('option');
+                    opt.text = value.abbreviation;
+                    opt.value = value.abbreviation;
+                    US.appendChild(opt);
+                }  
+                    
+                }
+            };
+            xmlHttp.send();
+        }
+}
